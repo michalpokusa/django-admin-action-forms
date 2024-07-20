96f98e8c332c1cbd347d6246e19bfd816107a02d
michalpokusa <72110769+michalpokusa@users.noreply.github.com>
2024-07-20 23:55:54 +0000

    Added basic app files

---
+++ django_admin_action_forms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoAdminActionFormsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_admin_action_forms'
